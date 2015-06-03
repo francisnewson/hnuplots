80305d0cedf1417f24a8a4c26315debe7f6e7684
Francis Newson <fon@hep.ph.bham.ac.uk>
2015-06-03 19:34:02 +0100

    Blue field and k2pi plots

--- k2pi_plots/scripts/produce_plots.c
+++ k2pi_plots/scripts/produce_plots.c
@@ -69,8 +69,162 @@ void run()
 
         legd.Draw("same");
 
-        c.SaveAs( "output/chi2.pdf", "pdf" );
+        TArrow arrow( 0.5, 3, 0.5, 0.3, 0.03, "|>" );
+        arrow.SetLineColor( kWhite );
+        arrow.SetFillColor( kWhite );
+        arrow.SetFillStyle(1001);
+        arrow.SetLineWidth( 4 );
+        arrow.Draw();
 
+        c.SaveAs( "output/chi2.pdf", "pdf" );
+    }
+
+    {
+        THStack * eop_stack = 0;
+        tfin.GetObject( "no_fit_eop/hnu_stack", eop_stack );
+        if ( ! eop_stack )
+        { std::cout
+            << "Can't find : " <<  "no_fit_eop/hnu_stack" << std::endl; }
+
+        TCanvas c( "c", "c", 400, 400 );
+
+        eop_stack->Draw();
+        eop_stack->GetXaxis()->SetTitle( "E/p(#pi^{+})");
+        eop_stack->GetXaxis()->SetNdivisions( 508 , kTRUE);
+        eop_stack->SetMaximum( 3e5 );
+        eop_stack->SetMinimum( 10 );
+
+
+        c.Update();
+        c.SetGrid( 1, 1);
+        c.SetLogy();
+
+        TList * hlist = eop_stack->GetHists();
+        TLegend legd(0.70, 0.57, 0.87, 0.87 );
+        legd.SetTextFont( 133 );
+        legd.SetTextSize( 14 );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k2pig_pos") ) , "K_{2#pi}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "ke3_pos") ) , "K_{e3}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k3pi0_pos") ) , "K_{3#pi0}", "f"  );
+
+        legd.Draw("same");
+
+        TArrow arrow( 0.9, 30, 0.9, 3, 0.03, "|>" );
+        arrow.SetLineColor( kWhite );
+        arrow.SetFillColor( kWhite );
+        arrow.SetFillStyle(1001);
+        arrow.SetLineWidth( 4 );
+        arrow.Draw();
+
+        c.SaveAs( "output/eop.pdf", "pdf" );
+    }
+
+    {
+        int rebin = 5;
+
+        THStack * m2pi0_stack = 0;
+        tfin.GetObject( "fit_eop_90_dm2_pi0/hnu_stack", m2pi0_stack );
+        if ( ! m2pi0_stack )
+        { std::cout
+            << "Can't find : " <<  "fit_eop_90_dm2_pi0/hnu_stack" << std::endl; }
+
+        TH1D * hdata =  0;
+        tfin.GetObject( "fit_eop_90_dm2_pi0/hdata", hdata );
+        if ( ! hdata ) {
+            std::cout << "Can't find : " <<  "fit_eop_90_dm2_pi0/hdata" << std::endl; 
+        return;
+        }
+        hdata->Rebin(rebin);
+
+        TCanvas c( "c", "c", 400, 400 );
+
+        TList * hlist = m2pi0_stack->GetHists();
+        TIter iter( hlist );
+        for( TIter& hit = iter.Begin() ; hit != TIter::End() ; ++hit )
+        {
+            std::cout << (*hit)->GetName() << std::endl;
+            static_cast<TH1*>( *hit )->Rebin(rebin);
+        }
+
+        m2pi0_stack->Draw();
+        m2pi0_stack->GetXaxis()->SetTitle( "Reconstruced m^{2}_{#pi^{0}} (GeV^{2}/c^{4})");
+        m2pi0_stack->GetXaxis()->SetNdivisions( 508 , kTRUE);
+        m2pi0_stack->GetXaxis()->SetRangeUser( -0.2 , 0.15);
+        m2pi0_stack->SetMaximum( 3e5 );
+        m2pi0_stack->SetMinimum( 1 );
+
+        c.Update();
+        c.SetGrid( 1, 1);
+        c.SetLogy();
+
+        hdata->Draw("same, E1" );
+
+
+        TLegend legd(0.70, 0.57, 0.87, 0.87 );
+        legd.SetTextFont( 133 );
+        legd.SetTextSize( 14 );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k2pig_pos") ) , "K_{2#pi}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "ke3_pos") ) , "K_{e3}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k3pi0_pos") ) , "K_{3#pi0}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "km3_pos") ) , "K_{#mu3}", "f"  );
+        legd.AddEntry( hdata , "data", "lpe"  );
+
+        legd.Draw("same");
+        c.SaveAs( "output/m2pi0.pdf", "pdf" );
+    }
+
+    {
+        int rebin = 2;
+
+        THStack * pt_stack = 0;
+        tfin.GetObject( "fit_eop_90_pt/hnu_stack", pt_stack );
+        if ( ! pt_stack )
+        { std::cout
+            << "Can't find : " <<  "fit_eop_90_pt/hnu_stack" << std::endl; }
+
+        TH1D * hdata =  0;
+        tfin.GetObject( "fit_eop_90_pt/hdata", hdata );
+        if ( ! hdata ) {
+            std::cout << "Can't find : " <<  "fit_eop_90_pt/hdata" << std::endl; 
+        return;
+        }
+        hdata->Rebin(rebin);
+
+        TCanvas c( "c", "c", 400, 400 );
+
+        TList * hlist = pt_stack->GetHists();
+        TIter iter( hlist );
+        for( TIter& hit = iter.Begin() ; hit != TIter::End() ; ++hit )
+        {
+            std::cout << (*hit)->GetName() << std::endl;
+            static_cast<TH1*>( *hit )->Rebin(rebin);
+        }
+
+        pt_stack->Draw();
+        pt_stack->GetXaxis()->SetTitle( "E/p");
+        pt_stack->GetXaxis()->SetTitle( "p_{T}(#pi^{+}) (GeV/c)");
+        pt_stack->GetXaxis()->SetNdivisions( 508 , kTRUE);
+        pt_stack->SetMaximum( 3e5 );
+        pt_stack->SetMinimum( 1 );
+
+        c.Update();
+        c.SetGrid( 1, 1);
+        c.SetLogy();
+
+        hdata->Draw("same, E1" );
+
+
+        TLegend legd(0.70, 0.57, 0.87, 0.87 );
+        legd.SetTextFont( 133 );
+        legd.SetTextSize( 14 );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k2pig_pos") ) , "K_{2#pi}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "ke3_pos") ) , "K_{e3}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "k3pi0_pos") ) , "K_{3#pi0}", "f"  );
+        legd.AddEntry( static_cast<TH1*>( hlist->FindObject( "km3_pos") ) , "K_{#mu3}", "f"  );
+        legd.AddEntry( hdata , "data", "lpe"  );
+
+        legd.Draw("same");
+        c.SaveAs( "output/pt.pdf", "pdf" );
     }
 
 
